9e0c6c3fe05e92927f26f52a0eacfeb8368a35ab
lingyi <yhl990104@qq.com>
2022-07-08 15:46:06 +0800

    $'2022/7/8 15:46:05'

--- scripts/sync.mjs
+++ scripts/sync.mjs
@@ -3,7 +3,9 @@ import 'zx/globals'
 let branch = await $`git branch --show-current`
 const currTime = (new Date()).toLocaleString()
 
-await $`git add .`;
-await $`git commit -m "${currTime}"`;
-await $`git push`;
-console.log(`上传到github成功`)
+await Promise.all([
+  $`git add .`,
+  $`git commit -m "${currTime}"`,
+  $`git push`
+])
+console.log(`成功同步到github!`)
