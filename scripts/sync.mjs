88293a046474babfeae0f01b709f3e9910f65f51
lingyi <yhl990104@qq.com>
2022-07-08 15:48:23 +0800

    2022/7/8 15:48:22

--- scripts/sync.mjs
+++ scripts/sync.mjs
@@ -4,6 +4,6 @@ let branch = await $`git branch --show-current`
 const currTime = (new Date()).toLocaleString()
 
 await $`git add .`;
-await $`git commit -m "${currTime}"`;
+await $`git commit -m ${currTime}`;
 await $`git push`;
 console.log(`成功同步到github!`)
