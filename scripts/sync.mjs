062273b6482f4467c11ec9b22b48e6a927c109f2
lingyi <yhl990104@qq.com>
2022-07-08 15:42:26 +0800

    $'2022/7/8 15:42:26'

---
+++ scripts/sync.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+let branch = await $`git branch --show-current`
+const currTime = (new Date()).toLocaleString()
+
+await $`git add .`;
+await $`git commit -m "${currTime}"`;
+await $`git push`;
+console.log(`上传到github成功`)
